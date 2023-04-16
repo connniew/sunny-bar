ba3cd06d9ab8916e6cd86ced8c30bed04bc448b5
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-04-15 23:51:19 -0400

    Convert to php

--- home.php
+++ home.php
@@ -5,12 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sunny Bar</title>
+
+    <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700;900&display=swap" rel="stylesheet">
 </head>
 <body>
     <img src="images/logo.png" alt="">
+    <img src="images/logoOrange.png" alt="">
+    <img src="images/logoRed.png" alt="">
+
     
 </body>
 </html>
