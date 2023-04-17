c2ba1d94db7fe7706cb3af66f47069513cd8e36f
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-04-16 21:04:32 -0400

    Added new logo options

--- home.php
+++ home.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Sunny Bar</title>
 
     <!-- Google Fonts -->
@@ -11,6 +12,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700;900&display=swap" rel="stylesheet">
 
+    <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon_io/favicon-32x32.png">
     <link rel="icon" type="images/png" sizes="16x16" href="images/favicon_io/favicon-16x16.png">
@@ -22,7 +24,5 @@
     <img src="images/logoOrange2.png" alt="">
     <img src="images/logoRed.png" alt="">
     <img src="images/logoRed2.png" alt="">
-
-    
 </body>
 </html>
