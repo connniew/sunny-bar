5346da5b13128f06c51690ccc69f7aafc44abcb4
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-04-16 22:04:02 -0400

    Changed favicon

--- home.php
+++ home.php
@@ -12,17 +12,14 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700;900&display=swap" rel="stylesheet">
 
-    <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon_io/favicon-32x32.png">
-    <link rel="icon" type="images/png" sizes="16x16" href="images/favicon_io/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.png">
+<link rel="manifest" href="images/favicons/site.webmanifest">
 
 </head>
 <body>
-    <img src="images/logoOrange.png" alt="">
     <img src="images/logoOrange2.png" alt="">
-    <img src="images/logoRed.png" alt="">
-    <img src="images/logoRed2.png" alt="">
+    <img src="images/logo2.png" alt="">
 </body>
 </html>
