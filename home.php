3f54715a640c8e1a18edc004b0a08605d81caaa1
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-04-17 23:01:35 -0400

    Added favicon and logo to home

--- home.php
+++ home.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
+<!-- #fdf49a -->
+<!-- #364F6B -->
     
 <?php include 'header.php';?>
 
 <body>
-    <img src="images/logo.png" alt="">
+    <img src="images/logo-name-black.png" alt="">
+    <img src="images/logo-name-blue.png" alt="">
+    <img src="images/logo-name-white.png" alt="">
+    <img src="images/logo-round-white.png" alt="">
+    <img src="images/logo-round-yellow.png" alt="">
+    <img src="images/logo-sun-black.png" alt="">
+    <img src="images/logo-sun-white.png" alt="">
+    <img src="images/logo-sun-yellow.png" alt="">
 </body>
 
 </html>
