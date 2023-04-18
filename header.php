d748553f5d8c47b9795c18506681f9486708d75b
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-04-18 00:16:43 -0400

    Updated favicon with transparent background

--- header.php
+++ header.php
@@ -12,10 +12,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700;900&display=swap" rel="stylesheet">
 
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="images/faviconio/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/faviconio/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images/faviconio/favicon-16x16.png">
-    <link rel="manifest" href="images/faviconio/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favico/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favico/favicon-16x16.png">
+    <link rel="manifest" href="images/favico/site.webmanifest">
 
     <link rel="stylesheet" href="css/styles.css">
 </head>
