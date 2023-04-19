9c8d010894ee5171d87a190565a7d9f92baa7bca
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-04-18 23:44:48 -0400

    Added SASS

--- header.php
+++ header.php
@@ -17,5 +17,5 @@
     <link rel="icon" type="image/png" sizes="16x16" href="images/favico/favicon-16x16.png">
     <link rel="manifest" href="images/favico/site.webmanifest">
 
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles.css?<?php echo time(); ?>">
 </head>
--- home.php
+++ home.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- #fdf49a -->
+<!-- Yellow #fdf49a -->
 <!-- #364F6B -->
     
 <?php include 'header.php';?>
 
 <body>
+    <header>
+        
+    </header>
     <img src="images/logo-desktop-black.png" alt="">
     <img src="images/logo-desktop-blue.png" alt="">
     <img src="images/logo-mobile-black.png" alt="">
