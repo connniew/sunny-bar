ff05d57aacaf532bdfdb4e44347fdffafd9cf23f
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-04-23 22:24:21 -0400

    Added mobile and desktop version of navbar into one nav

--- navbar.php
+++ navbar.php
@@ -1,18 +1,10 @@
-<nav class="navbar mobile flex">
-    <div class="logo">
-        <img src="images/logo-mobile.png" class="mobile" alt="logo">
-    </div>
-    <div class="menu">
-        <i class="fa-solid fa-bars fa-2xl"></i>
-    </div>
-</nav>
-
 <nav class="navbar desktop flex hide">
-    <div class="logo">
-        <img src="images/logo-desktop.png" class="desktop" alt="logo">
-    </div>
-    <div class="menu">
-        <ul class="flex">
+
+    <div class="mobile">
+        <div class="logo">
+            <img src="images/logo-desktop.png" class="desktop" alt="logo">
+        </div>
+        <ul class="menu flex">
             <li><a href="">Home</a></li>
             <li><a href="">About</a></li>
             <li><a href="">Shop Online</a></li>
@@ -21,4 +13,19 @@
             <li><a href="">Shopping Cart</a></li>
         </ul>
     </div>
+
+    <div class="desktop">
+        <div class="logo">
+            <img src="images/logo-desktop.png" class="desktop" alt="logo">
+        </div>
+        <ul class="menu flex">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Shop Online</a></li>
+            <li><a href="">Find Locations</a></li>
+            <li><a href="">Login/Sign Up</a></li>
+            <li><a href="">Shopping Cart</a></li>
+        </ul>
+    </div>
+
 </nav>
