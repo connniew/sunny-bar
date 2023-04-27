b78e27bbef4d904be34990fee8fd6146ef37d6a8
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-04-27 00:10:31 -0400

    Removed logo for mobile
    Only one logo version is used for mobile and desktop

--- navbar.php
+++ navbar.php
@@ -1,20 +1,18 @@
 <nav class="navbar">
 
+    <img class="navbar__logo" src="images/logo-desktop.png" alt="logo">
+
     <div class="navbar__mobile">
-        <img class="navbar__mobile__logo" src="images/logo-sun.png" alt="logo">
         <i class="navbar__mobile__menu fa-solid fa-bars fa-xl"></i>
     </div>
 
-    <div class="navbar__desktop">
-        <img class="navbar__desktop__logo" src="images/logo-desktop.png" alt="logo">
-        <ul class="navbar__desktop__menu">
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Shop Online</a></li>
-            <li><a href="">Find Locations</a></li>
-            <li><a href="">Login/Sign Up</a></li>
-            <li><a href="">Shopping Cart</a></li>
-        </ul>
-    </div>
+    <ul class="navbar__desktop__menu">
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Shop Online</a></li>
+        <li><a href="">Find Locations</a></li>
+        <li><a href="">Login/Sign Up</a></li>
+        <li><a href="">Shopping Cart</a></li>
+    </ul>
 
 </nav>
