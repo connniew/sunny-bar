ef32648b4829017d313a670d07b840bf5de085fc
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-04-29 18:29:51 -0400

    Changed font styles
    Playfair + Lato

--- header.php
+++ header.php
@@ -9,8 +9,8 @@
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700;900&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Playfair+Display:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+    
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="images/favico/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="images/favico/favicon-32x32.png">
