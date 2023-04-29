e38d7c8802e3ca74007dbd80450fac114109fef3
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-04-29 18:33:56 -0400

    Changed logo and favicon

--- header.php
+++ header.php
@@ -12,9 +12,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Playfair+Display:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
     
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="images/favico/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favico/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favico/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/faviconio/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/faviconio/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/faviconio/favicon-16x16.png">
     <link rel="manifest" href="images/favico/site.webmanifest">
 
     <!-- Font Awesome -->
