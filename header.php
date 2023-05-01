75769c0734765f778ab6883fdde6710bfb1f06b4
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-05-01 16:40:42 -0400

    Moved favicon out of images

--- header.php
+++ header.php
@@ -15,10 +15,10 @@
     <script src="https://kit.fontawesome.com/4f6feac275.js" crossorigin="anonymous"></script>
     
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon_io/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon_io/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon_io/favicon-16x16.png">
-    <link rel="manifest" href="images/favicon_io/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="favicon_io/site.webmanifest">
 
     <!-- css -->
     <link rel="stylesheet" href="styles/global.css?<?php echo time(); ?>">
