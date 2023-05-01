50c794a3354a5181f271644bf0ff5c0ac20daefd
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-05-01 16:10:06 -0400

    Added favicon

--- header.php
+++ header.php
@@ -14,6 +14,12 @@
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/4f6feac275.js" crossorigin="anonymous"></script>
     
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="images/favicon_io/site.webmanifest">
+
     <!-- css -->
     <link rel="stylesheet" href="styles/global.css?<?php echo time(); ?>">
     <link rel="stylesheet" href="styles/navbar.css?<?php echo time(); ?>">
