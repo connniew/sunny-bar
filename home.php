3c8715936a434a518caf7d7a5e9567b7d16bd9ff
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-05-01 20:44:32 -0400

    Reverted classes

--- home.php
+++ home.php
@@ -11,13 +11,13 @@
 
     <main class="main-content">
         
-        <section class="main-content__cta">
-            <h1 class="main-content__heading">Indulge in handcrafted ice cream perfection</h1>
-            <h2 class="main-content__subheading">Experience the creamy, rich flavors of our artisanal creations</h2>
-            <a class="main-content__btn" href="">Discover our flavours</a>
+        <section class="cta">
+            <h1 class="cta__heading">Indulge in handcrafted ice cream perfection</h1>
+            <h2 class="cta__subheading">Experience the creamy, rich flavors of our artisanal creations</h2>
+            <a class="cta__btn" href="">Discover our flavours</a>
         </section>
         
-        <picture class="main-content__img">
+        <picture class="home-image">
             <source media="(min-width: 992px)" srcset="images/home.jpg">
             <source media="(min-width: 768px)" srcset="images/home-tablet.jpg">
             <img src="images/home.jpg" alt="store image">
