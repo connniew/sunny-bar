1c281182dea3706d3fe940d1acc62cd8458184b4
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-05-02 23:03:23 -0400

    Created flavours files

---
+++ flavours.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<?php include 'header.php';?>
+<link rel="stylesheet" href="styles/flavours.css?<?php echo time(); ?>">
+
+<body>
+    <header>
+        <?php include 'navbar.php';?>
+    </header>
+
+    <main class="main-content">
+        
+    </main>
+    
+    <?php include 'footer.php';?>
+</body>
+
+</html>
