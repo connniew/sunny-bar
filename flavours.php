a15a9725a922cf2cad1b023ea8cc9eb0b3175712
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-05-03 23:37:02 -0400

    Added flavours and images

--- flavours.php
+++ flavours.php
@@ -12,81 +12,81 @@
     <main class="main-content">
         <h1>All Flavours</h1>
         <div class="flavour">
-            <img src="" alt="">
+            <img src="images/lavenderhoney.jpg" alt="">
             <h2>Lavender Honey</h2>
             <p>A floral and sweet combination that's both soothing and refreshing.</p>
         </div>
         <div class="flavour">
-            <img src="" alt="">
-            <h2>Blackberry Sage</h2>
-            <p>A tangy and refreshing flavor that combines the tartness of blackberries with the earthy flavor of sage.</p>
+            <img src="images/raspberrymint.jpg" alt="">
+            <h2>Raspberry Mint</h2>
+            <p>Tart raspberries and cool mint come together in this refreshing and vibrant sorbet flavor.</p>
         </div>
         <div class="flavour">
-            <img src="" alt="">
+            <img src="images/bourbobrownsugar.jpg" alt="">
             <h2>Bourbon Brown Sugar</h2>
             <p>A decadent flavor that combines the smoky taste of bourbon with the sweetness of brown sugar.</p>
         </div>
 
         <div class="flavour">
-            <img src="" alt="">
+            <img src="images/matchawhitechocolate.jpg" alt="">
             <h2>Matcha White Chocolate</h2>
             <p>A delicate and luxurious ice cream with a hint of earthy matcha and chunks of smooth white chocolate.</p>
         </div>
         <div class="flavour">
-            <img src="" alt="">
+            <img src="images/earlgreytea.jpg" alt="">
             <h2>Earl Grey Tea</h2>
             <p>A sophisticated flavor that combines the floral taste of Earl Grey tea with the creaminess of ice cream.</p>
         </div>
         <div class="flavour">
-            <img src="" alt="">
+            <img src="images/honeycomb.jpg" alt="">
             <h2>Honeycomb</h2>
             <p>A sweet and crunchy ice cream featuring chunks of golden, caramelized honeycomb.</p>
         </div>
 
         <div class="flavour">
-            <img src="" alt="">
+            <img src="images/rosewaterpistachio.jpg" alt="">
             <h2>Rosewater Pistachio</h2>
             <p>A delicate floral ice cream with a nutty pistachio finish.</p>
         </div>
         <div class="flavour">
-            <img src="" alt="">
-            <h2>Mango Chili Lime</h2>
-            <p>A tropical and spicy ice cream with a kick of chili heat and a tangy twist of lime.</p>
+            <img src="images/peachginger.jpg" alt="">
+            <h2>Peach Ginger</h2>
+            <p>Ripe peaches and spicy ginger come together in this refreshing and unique sorbet flavor.</p>
         </div>
         <div class="flavour">
-            <img src="" alt="">
+            <img src="images/cherrycheesecake.jpg" alt="">
             <h2>Cherry Cheesecake</h2>
             <p>A creamy and tangy ice cream with swirls of sweet cherry sauce and chunks of rich cheesecake.</p>
         </div>
 
         <div class="flavour">
-            <img src="" alt="">
+            <img src="images/blueberrycheesecake.jpg" alt="">
             <h2>Blueberry Cheesecake</h2>
             <p>A creamy and tangy ice cream loaded with chunks of cheesecake and fresh blueberries.</p>
         </div>
         <div class="flavour">
-            <img src="" alt="">
-            <h2>Caramelized Fig</h2>
-            <p>A velvety and decadent ice cream made with caramelized figs and a drizzle of honey.</p>
+            <img src="images/bloodorangegrapefruit.jpg" alt="">
+            <h2>Blood Orange Grapefruit</h2>
+            <p>This tangy combination of citrus flavors is bright, refreshing, and full of zesty flavor.</p>
         </div>
         <div class="flavour">
-            <img src="" alt="">
-            <h2>Pistachio Rose</h2>
-            <p>A luxurious and aromatic ice cream made with roasted pistachios and delicate rose petals.</p>
+            <img src="images/belgianchocolate.jpg" alt="">
+            <h2>Belgian Chocolate</h2>
+            <p>A decadent chocolate flavor made with the finest Belgian chocolate, creating a smooth and creamy texture.</p>
         </div>
 
         <div class="flavour">
-            <img src="" alt="">
-            <h2>Salted Caramel Popcorn</h2>
-            <p>A crunchy and indulgent ice cream made with salted caramel sauce and chunks of popcorn.</p>
+            <img src="images/blueberrylemon.jpg" alt="">
+            <h2>Blueberry Lemon</h2>
+            <p>Sweet and juicy blueberries paired with tart lemon make for a bright and refreshing sorbet flavor.</p>
         </div>
         <div class="flavour">
-            <img src="" alt="">
+            <img src="images/matchagreentea.jpg" alt="">
             <h2>Matcha Green Tea</h2>
             <p>A refreshing and antioxidant-rich ice cream made with premium Japanese matcha powder and sweet cream.</p>
         </div>
         <div class="flavour">
-            <img src="" alt="">
+            <img src="images/cardamomorange.jpg" alt="">
             <h2>Cardamom Orange</h2>
             <p>A bold and aromatic ice cream made with warming cardamom spice and zesty orange.</p>
         </div>
