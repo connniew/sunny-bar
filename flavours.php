3a6886a11b0352a6402a8c9b44aca1b6d7b3d03e
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-05-03 23:10:22 -0400

    Update flavours.php

--- flavours.php
+++ flavours.php
@@ -10,7 +10,86 @@
     </header>
 
     <main class="main-content">
-        
+        <h1>All Flavours</h1>
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Lavender Honey</h2>
+            <p>A floral and sweet combination that's both soothing and refreshing.</p>
+        </div>
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Blackberry Sage</h2>
+            <p>A tangy and refreshing flavor that combines the tartness of blackberries with the earthy flavor of sage.</p>
+        </div>
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Bourbon Brown Sugar</h2>
+            <p>A decadent flavor that combines the smoky taste of bourbon with the sweetness of brown sugar.</p>
+        </div>
+
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Matcha White Chocolate</h2>
+            <p>A delicate and luxurious ice cream with a hint of earthy matcha and chunks of smooth white chocolate.</p>
+        </div>
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Earl Grey Tea</h2>
+            <p>A sophisticated flavor that combines the floral taste of Earl Grey tea with the creaminess of ice cream.</p>
+        </div>
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Honeycomb</h2>
+            <p>A sweet and crunchy ice cream featuring chunks of golden, caramelized honeycomb.</p>
+        </div>
+
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Rosewater Pistachio</h2>
+            <p>A delicate floral ice cream with a nutty pistachio finish.</p>
+        </div>
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Mango Chili Lime</h2>
+            <p>A tropical and spicy ice cream with a kick of chili heat and a tangy twist of lime.</p>
+        </div>
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Cherry Cheesecake</h2>
+            <p>A creamy and tangy ice cream with swirls of sweet cherry sauce and chunks of rich cheesecake.</p>
+        </div>
+
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Blueberry Cheesecake</h2>
+            <p>A creamy and tangy ice cream loaded with chunks of cheesecake and fresh blueberries.</p>
+        </div>
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Caramelized Fig</h2>
+            <p>A velvety and decadent ice cream made with caramelized figs and a drizzle of honey.</p>
+        </div>
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Pistachio Rose</h2>
+            <p>A luxurious and aromatic ice cream made with roasted pistachios and delicate rose petals.</p>
+        </div>
+
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Salted Caramel Popcorn</h2>
+            <p>A crunchy and indulgent ice cream made with salted caramel sauce and chunks of popcorn.</p>
+        </div>
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Matcha Green Tea</h2>
+            <p>A refreshing and antioxidant-rich ice cream made with premium Japanese matcha powder and sweet cream.</p>
+        </div>
+        <div class="flavour">
+            <img src="" alt="">
+            <h2>Cardamom Orange</h2>
+            <p>A bold and aromatic ice cream made with warming cardamom spice and zesty orange.</p>
+        </div>
     </main>
     
     <?php include 'footer.php';?>
