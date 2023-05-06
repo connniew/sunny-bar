8ca81ced74f339c74aace91b5384ab0a1f1e6b18
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-05-05 23:22:12 -0400

    Added flavours name description styles

--- flavours.php
+++ flavours.php
@@ -15,96 +15,96 @@
             <div class="flavours__flavour">
                 <!-- <img src="images/lavenderhoney.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Lavender Honey</h2>
-                <p>A floral and sweet combination that's both soothing and refreshing.</p>
+                <h2 class="flavours__name">Lavender Honey</h2>
+                <p class="flavours__description">A floral and sweet combination that's both soothing and refreshing.</p>
             </div>
             <div class="flavours__flavour">
                 <!-- <img src="images/raspberrymint.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Raspberry Mint</h2>
-                <p>Tart raspberries and cool mint come together in this refreshing and vibrant sorbet flavor.</p>
+                <h2 class="flavours__name">Raspberry Mint</h2>
+                <p class="flavours__description">Tart raspberries and cool mint come together in this refreshing and vibrant sorbet flavor.</p>
             </div>
             <div class="flavours__flavour">
                 <!-- <img src="images/bourbobrownsugar.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Bourbon Brown Sugar</h2>
-                <p>A decadent flavor that combines the smoky taste of bourbon with the sweetness of brown sugar.</p>
+                <h2 class="flavours__name">Bourbon Brown Sugar</h2>
+                <p class="flavours__description">A decadent flavor that combines the smoky taste of bourbon with the sweetness of brown sugar.</p>
             </div>
 
             <div class="flavours__flavour">
                 <!-- <img src="images/matchawhitechocolate.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Matcha White Chocolate</h2>
-                <p>A delicate and luxurious ice cream with a hint of earthy matcha and chunks of smooth white chocolate.</p>
+                <h2 class="flavours__name">Matcha White Chocolate</h2>
+                <p class="flavours__description">A delicate and luxurious ice cream with a hint of earthy matcha and chunks of smooth white chocolate.</p>
             </div>
             <div class="flavours__flavour">
                 <!-- <img src="images/earlgreytea.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Earl Grey Tea</h2>
-                <p>A sophisticated flavor that combines the floral taste of Earl Grey tea with the creaminess of ice cream.</p>
+                <h2 class="flavours__name">Earl Grey Tea</h2>
+                <p class="flavours__description">A sophisticated flavor that combines the floral taste of Earl Grey tea with the creaminess of ice cream.</p>
             </div>
             <div class="flavours__flavour">
                 <!-- <img src="images/honeycomb.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Honeycomb</h2>
-                <p>A sweet and crunchy ice cream featuring chunks of golden, caramelized honeycomb.</p>
+                <h2 class="flavours__name">Honeycomb</h2>
+                <p class="flavours__description">A sweet and crunchy ice cream featuring chunks of golden, caramelized honeycomb.</p>
             </div>
 
             <div class="flavours__flavour">
                 <!-- <img src="images/rosewaterpistachio.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Rosewater Pistachio</h2>
-                <p>A delicate floral ice cream with a nutty pistachio finish.</p>
+                <h2 class="flavours__name">Rosewater Pistachio</h2>
+                <p class="flavours__description">A delicate floral ice cream with a nutty pistachio finish.</p>
             </div>
             <div class="flavours__flavour">
                 <!-- <img src="images/cherrycheesecake.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Cherry Cheesecake</h2>
-                <p>A creamy and tangy ice cream with swirls of sweet cherry sauce and chunks of rich cheesecake.</p>
+                <h2 class="flavours__name">Cherry Cheesecake</h2>
+                <p class="flavours__description">A creamy and tangy ice cream with swirls of sweet cherry sauce and chunks of rich cheesecake.</p>
             </div>
             <div class="flavours__flavour">
                 <!-- <img src="images/peachginger.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Peach Ginger</h2>
-                <p>Ripe peaches and spicy ginger come together in this refreshing and unique sorbet flavor.</p>
+                <h2 class="flavours__name">Peach Ginger</h2>
+                <p class="flavours__description">Ripe peaches and spicy ginger come together in this refreshing and unique sorbet flavor.</p>
             </div>
 
             <div class="flavours__flavour">
                 <!-- <img src="images/blueberrycheesecake.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Blueberry Cheesecake</h2>
-                <p>A creamy and tangy ice cream loaded with chunks of cheesecake and fresh blueberries.</p>
+                <h2 class="flavours__name">Blueberry Cheesecake</h2>
+                <p class="flavours__description">A creamy and tangy ice cream loaded with chunks of cheesecake and fresh blueberries.</p>
             </div>
             <div class="flavours__flavour">
                 <!-- <img src="images/bloodorangegrapefruit.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Blood Orange Grapefruit</h2>
-                <p>This tangy combination of citrus flavors is bright, refreshing, and full of zesty flavor.</p>
+                <h2 class="flavours__name">Blood Orange Grapefruit</h2>
+                <p class="flavours__description">This tangy combination of citrus flavors is bright, refreshing, and full of zesty flavor.</p>
             </div>
             <div class="flavours__flavour">
                 <!-- <img src="images/saltedcaramel.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Salted Caramel</h2>
-                <p>A buttery caramel flavor with a touch of sea salt, creating a perfect balance of sweet and salty.</p>
+                <h2 class="flavours__name">Salted Caramel</h2>
+                <p class="flavours__description">A buttery caramel flavor with a touch of sea salt, creating a perfect balance of sweet and salty.</p>
             </div>
 
             <div class="flavours__flavour">
                 <!-- <img src="images/blueberrylemon.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Blueberry Lemon</h2>
-                <p>Sweet and juicy blueberries paired with tart lemon make for a bright and refreshing sorbet flavor.</p>
+                <h2 class="flavours__name">Blueberry Lemon</h2>
+                <p class="flavours__description">Sweet and juicy blueberries paired with tart lemon make for a bright and refreshing sorbet flavor.</p>
             </div>
             <div class="flavours__flavour">
                 <!-- <img src="images/cherryblossom.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Cherry Blossom</h2>
-                <p>A delicate flavor made with cherry blossom petals, giving it a floral, slightly sweet taste</p>
+                <h2 class="flavours__name">Cherry Blossom</h2>
+                <p class="flavours__description">A delicate flavor made with cherry blossom petals, giving it a floral, slightly sweet taste</p>
             </div>
             <div class="flavours__flavour">
                 <!-- <img src="images/cardamomorange.jpg" alt=""> -->
                 <div class="flavours__image"></div>
-                <h2>Cardamom Orange</h2>
-                <p>A bold and aromatic ice cream made with warming cardamom spice and zesty orange.</p>
+                <h2 class="flavours__name">Cardamom Orange</h2>
+                <p class="flavours__description">A bold and aromatic ice cream made with warming cardamom spice and zesty orange.</p>
             </div>
         </section>
     </main>
