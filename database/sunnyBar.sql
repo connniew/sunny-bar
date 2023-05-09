015a0d0879bd125fd6253adfc6f5d2bfca892a79
hiimcon <128109170+hiimcon@users.noreply.github.com>
2023-05-08 22:28:51 -0400

    Added product description and inserted products

--- database/sunnyBar.sql
+++ database/sunnyBar.sql
@@ -18,6 +18,7 @@ ALTER TABLE Customer AUTO_INCREMENT=10;
 CREATE TABLE Product (
   productID INT AUTO_INCREMENT, 
   productName VARCHAR(50) UNIQUE,
+  productDescription VARCHAR(550) UNIQUE,
   productPrice DECIMAL(10,2),
   qtyInStock INT DEFAULT 0,
   PRIMARY KEY (productID)
@@ -56,13 +57,26 @@ ALTER TABLE Payment AUTO_INCREMENT=900;
 
 -- insert dummy data
 INSERT INTO Customer VALUES (NULL, 'Ever', 'Avery', 'con@con.com', '123 Sunny St', DEFAULT, 'Toronto', DEFAULT, 'M1M1M1', 'ever', 'avery');
-INSERT INTO Product VALUES (NULL, 'Earl Grey', 10.99, 50);
-INSERT INTO Product VALUES (NULL, 'London Fog', 9.99, 50);
-INSERT INTO Product VALUES (NULL, 'Lavender', 8.00, 50);
+
 INSERT INTO Orders VALUES (NULL, 10, 28.98, DEFAULT, NULL);
 INSERT INTO OrderProducts VALUES (800, 200, 1);
 INSERT INTO OrderProducts VALUES (800, 201, DEFAULT);
 INSERT INTO OrderProducts VALUES (800, 202, DEFAULT);
+INSERT INTO Product VALUES (NULL, 'Earl Grey Tea', "A sophisticated flavor that combines the floral taste of Earl Grey tea with the creaminess of ice cream.", 7.80, 50);
+INSERT INTO Product VALUES (NULL, 'Matcha White Chocolate', "A delicate and luxurious ice cream with a hint of earthy matcha and chunks of smooth white chocolate.", 9.50, 50);
+INSERT INTO Product VALUES (NULL, 'Blood Orange Grapefruit', "This tangy combination of citrus flavors is bright, refreshing, and full of zesty flavor.", 7.80, 50)
+INSERT INTO Product VALUES (NULL, 'Lavender Honey', "A floral and sweet combination that's both soothing and refreshing.", 7.80, 50);
+INSERT INTO Product VALUES (NULL, 'Cherry Blossom', "A delicate flavor made with cherry blossom petals, giving it a floral, slightly sweet taste.", 7.80, 50)
+INSERT INTO Product VALUES (NULL, 'Bourbon Brown Sugar', "A decadent flavor that combines the smoky taste of bourbon with the sweetness of brown sugar.", 8.99, 50);
+INSERT INTO Product VALUES (NULL, 'Raspberry Mint', "Tart raspberries and cool mint come together in this refreshing and vibrant sorbet flavor.", 7.80, 50);
+INSERT INTO Product VALUES (NULL, 'Honeycomb', "A sweet and crunchy ice cream featuring chunks of golden, caramelized honeycomb.", 7.80, 50);
+INSERT INTO Product VALUES (NULL, 'Rosewater Pistachio', "A delicate floral ice cream with a nutty pistachio finish.", 7.80, 50);
+INSERT INTO Product VALUES (NULL, 'Cherry Cheesecake', "A creamy and tangy ice cream with swirls of sweet cherry sauce and chunks of rich cheesecake.", 8.99, 50);
+INSERT INTO Product VALUES (NULL, 'Peach Ginger', "Ripe peaches and spicy ginger come together in this refreshing and unique sorbet flavor.", 7.80, 50)
+INSERT INTO Product VALUES (NULL, 'Blueberry Cheesecake', "A creamy and tangy ice cream loaded with chunks of cheesecake and fresh blueberries.", 8.99, 50)
+INSERT INTO Product VALUES (NULL, 'Salted Caramel', "A buttery caramel flavor with a touch of sea salt, creating a perfect balance of sweet and salty.", 7.80, 50)
+INSERT INTO Product VALUES (NULL, 'Blueberry Lemon', "Sweet and juicy blueberries paired with tart lemon make for a bright and refreshing sorbet flavor.", 7.80, 50)
+INSERT INTO Product VALUES (NULL, 'Cardamom Orange', "A bold and aromatic ice cream made with warming cardamom spice and zesty orange.", 7.80, 50)
 INSERT INTO Payment VALUES (NULL, 800, 28.98, DEFAULT);
 
 -- delete
